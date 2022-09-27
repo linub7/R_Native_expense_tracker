a2aa1b44b3319f464b3d9b5c014e5543b6ef71b7
a1a2a3a4a5 <linub7@gmail.com>
2022-09-27 12:17:15 +0330

    Initial Setup & add basic navigation

---
+++ screens/AllExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const AllExpenses = () => {
+  return <Text>AllExpenses</Text>;
+};
+
+export default AllExpenses;
---
+++ screens/ManageExpense.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const ManageExpense = () => {
+  return <Text>ManageExpense</Text>;
+};
+
+export default ManageExpense;
